9afc75076e963ea324b17ca15e2da6e316dbe00f
CrisAE <70533403+CrisAE@users.noreply.github.com>
2020-10-21 12:07:28 -0500

    P5: método Monte Carlo

---
+++ P5/P5-integral.R
@@ -0,0 +1,45 @@
+﻿#Práctica 5: método de Monte-Carlo. Código base en Ref. [1].
+
+muestras=c(10,100,1000,10000,100000,500000)
+replica=10
+inicio= -6
+final= -inicio
+aum=0.25
+x=seq(inicio,final,aum)
+valor=0.048834
+i=1
+porc=numeric(length(muestras*replica))
+suppressMessages(library(distr))
+f = function(x) { return(1 / (exp(x) + exp(-x))) }
+g = function(x) { return((2 / pi) * f(x)) }
+desde = 3
+hasta = 7
+cuantos = 500
+generador = r(AbscontDistribution(d = g)) # creamos un generador            
+parte = function() {
+  valores = generador(muestra)
+  return(sum(valores >= desde & valores <= hasta))
+}
+suppressMessages(library(doParallel))
+registerDoParallel(makeCluster(detectCores() - 1))
+
+for(muestra in muestras){
+  for(r in 1:replica){
+montecarlo = foreach(i = 1:cuantos, .combine=c) %dopar% parte()
+integral = sum(montecarlo) / (cuantos * muestra)
+resultado=((pi / 2) * integral)
+error=abs(((valor-resultado)/valor)*100)
+porc[i]=error
+print(resultado)
+print(error)
+i=i+1
+  }
+}
+stopImplicitCluster()
+
+res=matrix(porc,ncol=6,nrow=replica)
+png("p5-error.png")
+lbls=c("10","100","1000","10000","100000","500000")
+boxplot(res,col=rainbow(6),names=lbls,ylab="Porcentaje de error",xlab="Tamaño de muestra")
+abline(h=0.048834,col="red")
+graphics.off()
---
+++ P5/P5-pintura.R
@@ -0,0 +1,47 @@
+﻿#Práctica 5: método Monte-Carlo. Código base en Ref. [4,6].
+
+library(countcolors)
+library(jpeg)
+coca=readJPEG('C:/Users/Cristina BH/Documents/coca.jpg')
+print(coca)
+
+#Definición de rangos de color para usar en el paquete countcolors de R
+white.center = c(1, 1, 1)
+red.center=c(1,0,0)
+#Cuenta exacta de pixeles por cada rango de color definido con el paquete countcolors de R
+coca.white=countcolors::sphericalRange(coca, center = white.center, radius = 0.5, color.pixels = FALSE, plotting = TRUE, target.color="magenta")  
+blanco=coca.white$pixel.count
+coca.red=countcolors::sphericalRange(coca, center = red.center, radius = 0.3, color.pixels = FALSE, plotting = TRUE, target.color="blue")  
+rojo=coca.red$pixel.count
+
+#Si se asume que un pixel equivale a 10cm^2 del mural, y que un litro de pintura rinde 10m^2:
+pixeles=c(blanco,rojo)
+pixeles
+pintura=pixeles*0.001 #Litros de pintura
+pintura
+
+#Estimación con método Monte-Carlo
+runs=1000
+datos=data.frame()
+for(r in 1:length(runs)){
+  for(s in 1:10000){
+    blue = coca[,,3]
+    x =sample (blue, runs[r])
+    y=sum(x < 0.5)
+    print(y)
+    datos=rbind(datos,c(s,runs[r],y))
+  }
+}
+names(datos)=c("Réplica","Muestra","Pixeles")
+write.table(datos,"datosp.txt",sep="\t",quote=F,row.names=F)
+pix = datos [,3]
+pixm = mean(pix)
+montecarlorojo = pixm*(160000/runs)
+montecarlorojo
+mcblanco=160000-montecarlorojo
+mcblanco
+#Litros de pintura
+pixelesmc=c(mcblanco,montecarlorojo)
+pixelesmc
+pinturamc=pixelesmc*0.001 
+pinturamc
